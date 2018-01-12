1c16ea1a228fa5184a2d1757c9d6a4c30c0e4bfa
rbleandro <rafaelbahialde@gmail.com>
2018-01-12 14:41:33 -0500

    delete uppercase folders

--- CDC/CarrinhoSku/200_Tabela_CarrinhoSku_CDISCOUNT.sql
+++
@@ -1,4 +0,0 @@
-
-IF NOT EXISTS (SELECT * FROM SYS.columns WHERE NAME = 'DataCriacao' AND object_id=object_id('CarrinhoSku'))
-ALTER TABLE CarrinhoSku ADD DataCriacao DATETIME NOT NULL CONSTRAINT DF_CarrinhoSku_DataCriacao DEFAULT getdate()
-GO
